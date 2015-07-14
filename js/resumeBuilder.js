36dd859272113818c818903764ac4c306d18145e
methmonk <magnus.andreasson82@gmail.com>
2015-07-14 10:45:08 +0200

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ var email = "cameron@udacity.com";
+ 
+ var newEmail = email.replace("udacity", "gmail");
+ 
+ console.log(email);
+ console.log(newEmail);
+ 
+ var awesomeThoughts = "My name is Magnus and I am AWESOME!";
+ 
+ var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+ 
+ $("main").append(funThoughts);
